2de732234b07ae9817d596d0832422062b22a281
Albert Einstein <javastraat@hotmail.com>
2025-07-29 15:44:34 +0000

    Revert "version"
    This reverts commit 528dee72ca847cc10425278adcb5f186c74582d8.

--- makedb.sh
+++
@@ -1,5 +0,0 @@
-rm radio_database.db
-python3 database-app-tool.py -i hamvoip hamvoip.json 
-python3 database-app-tool.py -i dapnet dapnet.json 
-python3 database-app-tool.py -i radioid radioid.json 
-python3 database-app-tool.py -i rptrs rptrs.json 
--- md5.sh
+++
@@ -1,6 +0,0 @@
-cd /home/einstein/software/dmr-database-appdata
-#md5sum *.json > checklist.chk  # generates a list of checksums for any file that matches *
-openssl md5 *.json > checklist.md5
-md5sum -c checklist.md5
-cat checklist.md5
-
